c042e3d14356471a94adfc27079aface2e7e7cb2
juicybacon <jason.baker79@outlook.com>
2018-11-03 12:57:10 +0000

    Update dice.rb
    Have added basic main code to roll the dice

--- dice.rb
+++ dice.rb
@@ -58,3 +58,41 @@ end
 
 #*******************************************************
 #Main code
+ans=""
+loop do
+	system "clear" or system "cls"
+	print "Would you like to roll the dice (Y/N)?"
+	loop do
+		ans=gets.chomp.capitalize!
+		if ans != nil then ans.strip!;end
+		puts ""
+		break if ans=="Y"
+		break if ans=="N"	
+	end
+	break if ans=="N"
+	num=rand(1..6)
+	case num
+		when 1
+			one
+		when 2
+			two
+		when 3
+			three
+		when 4
+			four
+		when 5
+			five
+		when 6
+			six
+	end
+	puts ""
+	print "Would you like to roll the dice again(Y/N)?"
+	loop do
+		ans=gets.chomp.capitalize!
+		if ans != nil then ans.strip;end
+		puts ""
+		break if ans=="Y"
+		break if ans=="N"	
+	end
+	break if ans=="N"
+end
