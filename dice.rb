42eb8402c43a701ebc0f30efe7879d63aa432b29
juicybacon <jason.baker79@outlook.com>
2018-11-03 12:43:51 +0000

    Update dice.rb
    Sides of the dice created

---
+++ dice.rb
@@ -0,0 +1,60 @@
+#A simple dice rolling program
+#Created by Jason Baker 2018
+#******************************************************
+#Methods
+def one
+	puts " ___________"
+	puts "|           |"
+	puts "|           |"
+	puts "|     *     |"
+	puts "|           |"
+	puts "|___________|"
+end
+
+def two
+	puts " ___________"
+	puts "|           |"
+	puts "|  *        |"
+	puts "|           |"
+	puts "|        *  |"
+	puts "|___________|"
+end
+
+def three
+	puts " ___________"
+	puts "|           |"
+	puts "|  *        |"
+	puts "|     *     |"
+	puts "|        *  |"
+	puts "|___________|"
+end
+
+def four
+	puts " ___________"
+	puts "|           |"
+	puts "|  *     *  |"
+	puts "|           |"
+	puts "|  *     *  |"
+	puts "|___________|"
+end
+
+def five
+	puts " ___________"
+	puts "|           |"
+	puts "|  *     *  |"
+	puts "|     *     |"
+	puts "|  *     *  |"
+	puts "|___________|"
+end
+
+def six
+	puts " ___________"
+	puts "|           |"
+	puts "|  *     *  |"
+	puts "|  *     *  |"
+	puts "|  *     *  |"
+	puts "|___________|"
+end
+
+#*******************************************************
+#Main code
